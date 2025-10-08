e8796262b3ad1963d4fbd5abb6b6ee0497aef26f
ThIsErIk7 <143787144+ThIsErIk7@users.noreply.github.com>
2025-10-08 19:10:29 +0200

    Update Main.java
    Update von Player intigration zum Test #1

--- src/main/java/com/risiko/Main.java
+++ src/main/java/com/risiko/Main.java
@@ -2,20 +2,44 @@ package com.risiko;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 import com.risiko.view.GameView;
+import com.risiko.model.Player;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) {
-        GameView gameView = new GameView();
-        Scene scene = new Scene(gameView, 1200, 800);
-        stage.setTitle("Risiko Light");
-        stage.setScene(scene);
-        stage.show();
-    }
+        // --- Player-Test ---
+        Player p1 = new Player("Erik", "Rot", 20);
+        System.out.println("=== Player Test ===");
+        System.out.println("Name: " + p1.getName());
+        System.out.println("Farbe: " + p1.getColor());
+        System.out.println("Armeen: " + p1.getArmies());
 
-    public static void main(String[] args) {
-        launch();
-    }
-}
+        // Verstärkung simulieren
+        p1.addArmies(5);
+        System.out.println("Nach Verstärkung: " + p1.getArmies());
+
+        // Verlust simulieren
+        p1.removeArmies(10);
+        System.out.println("Nach Angriff: " + p1.getArmies());
+        System.out.println("==================\n");
+
+                // --- GUI starten ---
+                // Falls du GameView schon hast:
+                // einfache leere Scene starten
+                Pane root = new Pane();
+                Scene scene = new Scene(root, 800, 600);
+                stage.setTitle("Risiko");
+                stage.setScene(scene);
+                stage.show();
+            }
+        
+            public static void main(String[] args) {
+                launch(args);
+            }
+        }
+        
